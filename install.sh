88914cad5d21fbe83da55084097045900577a51b
Uref <youbowtou6@gmail.com>
2023-08-21 15:47:14 +0900

    Remove xattr, open

--- install.sh
+++ install.sh
@@ -46,66 +46,16 @@ cp -f ./karabiner-elements/karabiner.json ~/.config/karabiner/karabiner.json
 mkdir -p ~/.ssh
 cp -f ./ssh/config ~/.ssh/config
 
-
+## ---
 # zsh (just before reboot)
 chmod 755 ./zsh/install.sh
 ./zsh/install.sh
 
 # reboot shell
 exec $SHELL -l
+## ---
+
 
 # skip security popup
-
-sudo xattr -dr com.apple.quarantine /Applications/Alfred\ 5.app/
-open /Applications/Alfred\ 5.app/
-sudo xattr -dr com.apple.quarantine /Applications/AltTab.app
-open /Applications/AltTab.app
-sudo xattr -dr com.apple.quarantine /Applications/BetterTouchTool.app
-open /Applications/BetterTouchTool.app
-sudo xattr -dr com.apple.quarantine /Applications/Bitwarden.app
-open /Applications/Bitwarden.app
-sudo xattr -dr com.apple.quarantine /Applications/Docker.app
-open /Applications/Docker.app
-sudo xattr -dr com.apple.quarantine /Applications/Dropbox.app
-open /Applications/Dropbox.app
-sudo xattr -dr com.apple.quarantine /Applications/Firefox.app
-open /Applications/Firefox.app
-sudo xattr -dr com.apple.quarantine /Applications/Google\ Chrome.app
-open /Applications/Google\ Chrome.app
-sudo xattr -dr com.apple.quarantine /Applications/IINA.app
-open /Applications/IINA.app
-sudo xattr -dr com.apple.quarantine /Applications/karabiner-elements.app
-open /Applications/karabiner-elements.app
-sudo xattr -dr com.apple.quarantine /Applications/Microsoft\ Excel.app
-open /Applications/Microsoft\ Excel.app
-sudo xattr -dr com.apple.quarantine /Applications/Microsoft\ Outlook.app
-open /Applications/Microsoft\ Outlook.app
-sudo xattr -dr com.apple.quarantine /Applications/Microsoft\ PowerPoint.app
-open /Applications/Microsoft\ PowerPoint.app
-sudo xattr -dr com.apple.quarantine /Applications/Microsoft\ Word.app
-open /Applications/Microsoft\ Word.app
-sudo xattr -dr com.apple.quarantine /Applications/Notion.app
-open /Applications/Notion.app
-sudo xattr -dr com.apple.quarantine /Applications/OneDrive.app
-open /Applications/OneDrive.app
-sudo xattr -dr com.apple.quarantine /Applications/PDF\ Expert.app
-open /Applications/PDF\ Expert.app
-sudo xattr -dr com.apple.quarantine /Applications/Reminders\ Menu\ Bar.app
-open /Applications/Reminders\ Menu\ Bar.app
-sudo xattr -dr com.apple.quarantine /Applications/Shifty.app
-open /Applications/Shifty.app
-sudo xattr -dr com.apple.quarantine /Applications/Slack.app
-open /Applications/Slack.app
-sudo xattr -dr com.apple.quarantine /Applications/The\ Unarchiver.app
-open /Applications/The\ Unarchiver.app
-sudo xattr -dr com.apple.quarantine /Applications/Visual\ Studio\ Code.app
-open /Applications/Visual\ Studio\ Code.app
-sudo xattr -dr com.apple.quarantine /Applications/iTerm.app
-open /Applications/iTerm.app
-sudo xattr -dr com.apple.quarantine /Applications/⌘英かな.app
-open /Applications/⌘英かな.app
-
-open /Applications/Bitwarden.app/
-open /Applications/GoodNotes.app/
-open /Applications/Focus.app/
-open /Applications/LINE.app/
+# sudo xattr -dr com.apple.quarantine /Applications/karabiner-elements.app
+# open /Applications/karabiner-elements.app
